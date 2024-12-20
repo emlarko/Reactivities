80c779ff200f295c47218f07fbfab0a08974fd5f
Emily Larkin <emily.larkin@sidetrade.com>
2024-12-20 16:44:20 +0000

    End of section 7

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './app/layout/App'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
+import { StoreContext, store } from './app/stores/store'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <StoreContext.Provider value={store}>
     <App />
+    </StoreContext.Provider>
   </StrictMode>,
 )
